4776702768f94c4cdc9bc90e27683550ed3c5015
kraqinc <275528312+kraqinc@users.noreply.github.com>
2026-08-16 17:31:57 -0500

    fix: remove duplicate AGP version declaration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level Android build configuration.
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
-    id("org.jetbrains.kotlin.kapt") version "2.4.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.google.services) apply false
 }
