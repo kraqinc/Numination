864661683e72566495aee4686e3c5befdd206708
wrenq <wrenq@localhost>
2026-09-22 19:04:23 -0500

    fix: update Android compileSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.wren.ide.numination"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
